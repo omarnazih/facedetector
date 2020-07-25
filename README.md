bacaa437265dc61f38b886a8a8120ac2c5f8688d
omar nazih <omarnazeh295@gmail.com>
2020-07-25 14:09:50 +0200

    test update

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # facedetector
 # this is a simple app to analyis the face detection proccess 
 # face detection - crop face - preprocessing - predction 
+
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import cv2
-
+from keras.preprocessing import image
 # Load the cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Read the input image
-img = cv2.imread('1.jpeg')
+img = cv2.imread('9.jpeg')
 # Convert into grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Detect faces
@@ -14,3 +14,20 @@ for (x, y, w, h) in faces:
 # Display the output
 cv2.imshow('img', crop)
 cv2.waitKey()
+
+
+
+gray2 = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
+
+
+x = image.img_to_array(gray2)
+x = np.expand_dims(x, axis = 0)
+
+x /= 255
+
+x = np.array(x, 'float32')
+x = x.reshape([48, 48]);
+
+print(x)
+
+
