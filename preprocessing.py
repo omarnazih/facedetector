6ae984d339fa6d4b3461bb629c80ead5cea2ea86
omar nazih <omarnazeh295@gmail.com>
2020-07-27 15:04:22 +0200

    udated preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -25,9 +25,28 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30,30),flags=cv2.CASCADE_SCALE_IMAGE)
     
-for (x, y, w, h) in faces:
-    roi = gray[y:y+h, x:x+w]
+#for (x, y, w, h) in faces:
+ #   roi = gray[y:y+h, x:x+w]
 
+
+
+    if len(faces) > 0:
+        faces = sorted(faces, reverse=True,
+        key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
+        (fX, fY, fW, fH) = faces
+        # Extract the ROI of the face from the grayscale image, resize it to a fixed 28x28 pixels, and then prepare
+        # the ROI for classification via the CNN
+        roi = gray[fY:fY + fH, fX:fX + fW]
+        roi = cv2.resize(roi, (64, 64))
+        roi = roi.astype("float") / 255.0
+        roi = img_to_array(roi)
+        roi = np.expand_dims(roi, axis=0)
+        
+        
+        preds = emotion_classifier.predict(roi)[0]
+        emotion_probability = np.max(preds)
+        label = EMOTIONS[preds.argmax()]
+    else: continue
 
 
 cv2.imshow('gray',roi)
